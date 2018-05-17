b7889092eb4920b24c2943fcab39e8db5d4bdaf2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-17 12:41:03 -0700

    CheckPoint 0

---
+++ src/CheckPoint.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class CheckPoint {
+public static void main(String[] args) {
+	
+	String answer = JOptionPane.showInputDialog("Hello, I am Alexa, what is your favorite color?");
+	JOptionPane.showMessageDialog(null, answer + " is also my favorite color!!");
+	
+	Robot Omega = new Robot();
+	if (color.equals)
+	
+}
+}
--- src/EverythingisAwesome.java
+++ src/EverythingisAwesome.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class EverythingisAwesome {
 public static void main(String[] args) {
 	
-	JOptionPane.showMessageDialog(nnull, "My name is Diego and I like video games, what do you like?");
+	JOptionPane.showMessageDialog(null, "My name is Diego and I like video games, what do you like?");
 	
 }
 }
